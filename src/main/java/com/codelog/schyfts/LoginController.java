0b4320f05fec76fc01bfbc27ca0904098399c2bb
Jaco Malan <jacomalan.12@gmail.com>
2020-12-29 14:26:09 +0200

    Added basic login screen

--- src/main/java/com/codelog/schyfts/LoginController.java
+++ src/main/java/com/codelog/schyfts/LoginController.java
@@ -7,7 +7,10 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.json.JSONObject;
 
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,13 +29,54 @@ public class LoginController implements Initializable {
         String javafxVersion = System.getProperty("javafx.version");
     }
 
-    public void btnLoginClick(ActionEvent actionEvent) {
+    public void btnLoginClick(ActionEvent actionEvent) throws IOException {
 
-        if (txtUname.getText().equals("jacom") && pwdPass.getText().equals("1234")) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Logged in!");
-            alert.setTitle("Logged in!");
-            alert.show();
-        }
+        String uname = txtUname.getText();
+        String pword = pwdPass.getText();
+
+        URL url = new URL("https://schyfts.uc.r.appspot.com/login");
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setDoOutput(true);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(con.getOutputStream()));
+
+        var body = new JSONObject();
+        body.put("uname", uname);
+        body.put("pword", pword);
+
+        System.out.println("Request body:");
+        System.out.println(body.toString());
+
+        writer.write(body.toString());
+        writer.flush();
+        writer.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("Response code: " + responseCode);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        StringBuilder builder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null)
+            builder.append(line);
+
+        System.out.println(builder.toString());
+
+        JSONObject response = new JSONObject(builder.toString());
+
+        var status = response.getString("status");
+        var message = response.getString("message");
+
+        con.disconnect();
+
+        Alert alert = new Alert(
+                (status.equals("ok")) ? Alert.AlertType.INFORMATION : Alert.AlertType.ERROR,
+                message
+        );
+
+        alert.setTitle("Message");
+        alert.show();
 
     }
 }
